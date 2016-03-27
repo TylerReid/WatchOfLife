5db7e025373a1618e84a3379a4358f5b7c141ad7
Tyler Reid <jamestylerreid@gmail.com>
2016-03-27 13:06:33 -0400

    Now drawing two rectangles

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_grid_layer;
+static Layer *s_cell_layer;
 static int* s_grid;
 
 char* text;
@@ -37,16 +38,36 @@ static void main_window_unload(Window * window) {
   
 }
 
+static void canvas_update_proc(Layer *layer, GContext *ctx) {
+  //this is where the drawing happens
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  GRect rect = GRect(10,10,20,20);
+  graphics_draw_rect(ctx, rect);
+  graphics_fill_rect(ctx, rect, 0, GCornerNone);
+  
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  rect = GRect(30,10,20,20);
+  graphics_draw_rect(ctx, rect);
+  graphics_fill_rect(ctx, rect, 0, GCornerNone);
+}
+
 static void init() {
   s_grid = createGrid();
   text = allocateText();
   s_main_window = window_create();
-  window_set_background_color(s_main_window, GColorBlack);
+  window_set_background_color(s_main_window, GColorTiffanyBlue);
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload,
   });
   window_stack_push(s_main_window, true);
+  //get the bounds of the window and create the cell layer
+  GRect bounds = layer_get_bounds(window_get_root_layer(s_main_window));
+  s_cell_layer = layer_create(bounds);
+  //add the callback for the drawing function
+  layer_set_update_proc(s_cell_layer, canvas_update_proc);
+  //add to the window
+  layer_add_child(window_get_root_layer(s_main_window), s_cell_layer);
 }
 
 static void deinit() {
