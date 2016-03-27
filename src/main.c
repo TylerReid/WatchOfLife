c8b0594a19af00b92e504f830aa839c57b39f8ca
Tyler Reid <jamestylerreid@gmail.com>
2016-03-27 17:46:40 -0400

    Fixed watch crash
    It was caused by the order of the timer event subscription and grid layer creation. Need to create layer before it is used in timer event, but for some reason emulator doesn't have this problem (timing?).

--- src/main.c
+++ src/main.c
@@ -3,38 +3,12 @@
 #include "render.h"
 
 static Window *s_main_window;
-static TextLayer *s_grid_layer;
 static Layer *s_cell_layer;
 static Grid s_grid;
-
-char* text;
 
 static void handle_second_tick(struct tm* tick_time, TimeUnits units) {
   simulate(s_grid);
   layer_mark_dirty(s_cell_layer);
-}
-
-static void main_window_load(Window *window) {
-  Layer *windowLayer = window_get_root_layer(window);
-  GRect bounds = layer_get_frame(windowLayer);
-  
-  s_grid_layer = text_layer_create(bounds);
-  text_layer_set_text_color(s_grid_layer, GColorWhite);
-  text_layer_set_background_color(s_grid_layer, GColorClear);
-  text_layer_set_font(s_grid_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text_alignment(s_grid_layer, GTextAlignmentCenter);
-  
-  time_t now = time(NULL);
-  struct tm *currentTime = localtime(&now);
-  handle_second_tick(currentTime, SECOND_UNIT);
-  
-  tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
-  
-  layer_add_child(windowLayer, text_layer_get_layer(s_grid_layer));
-}
-
-static void main_window_unload(Window * window) {
-  
 }
 
 static void canvas_update_proc(Layer *layer, GContext *ctx) {
@@ -42,16 +16,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   drawGrid(layer, ctx, s_grid);
 }
 
-static void init() {
-  s_grid = createGrid();
-  text = allocateText();
-  s_main_window = window_create();
-  window_set_background_color(s_main_window, GColorBlack);
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload,
-  });
-  window_stack_push(s_main_window, true);
+static void main_window_load(Window *window) {
   //get the bounds of the window and create the cell layer
   GRect bounds = layer_get_bounds(window_get_root_layer(s_main_window));
   s_cell_layer = layer_create(bounds);
@@ -59,11 +24,31 @@ static void init() {
   layer_set_update_proc(s_cell_layer, canvas_update_proc);
   //add to the window
   layer_add_child(window_get_root_layer(s_main_window), s_cell_layer);
+  
+  time_t now = time(NULL);
+  struct tm *currentTime = localtime(&now);
+  handle_second_tick(currentTime, SECOND_UNIT);
+  
+  tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
+}
+
+static void main_window_unload(Window * window) {
+  
+}
+
+static void init() {
+  s_grid = createGrid();
+  s_main_window = window_create();
+  window_set_background_color(s_main_window, GColorBlack);
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload,
+  });
+  window_stack_push(s_main_window, true);
 }
 
 static void deinit() {
   window_destroy(s_main_window);
-  free(text);
   free(s_grid.grid);
 }
 
